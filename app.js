0a5c02ba74f88e2a9c371ed073a7337e5016b195
Craig Bojko <craig@bojko.co.uk>
2016-05-31 22:03:11 +0100

    Updated app to route to client app and search endpoint

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var path = require('path')
 var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
-var config = require('./config/config')
+
 var app = express()
+var config = require('./config/config')
 var Foursquare = require('node-foursquare')
+var ac = ''
 
 app.use(cors())
 app.use(bodyParser.urlencoded({
@@ -21,6 +23,10 @@ var foursquare = Foursquare({
   }
 })
 
+app.get('/', function (req, res) {
+  res.redirect(301, 'http://localhost:' + config.port + '/login');
+})
+
 app.get('/login', function(req, res) {
   res.writeHead(303, { 'location': foursquare.getAuthClientRedirectUrl() })
   res.end()
@@ -33,10 +39,26 @@ app.get('/fsqRedirect', function (req, res) {
     if (error) {
       res.send('An error was thrown: ' + error.message)
     } else {
-      console.log(accessToken)
+      ac = accessToken
+      res.redirect(301, 'http://localhost:' + config.port + '/app');
     }
   })
 })
+
+app.get('/app', function (req, res) {
+  foursquare.Venues.search(null, null, 'Sidcup', {}, ac, function (err, results) {
+    console.log('SEARCH')
+    if (err) {
+      console.error(err)
+      res.send(500)
+    } else{
+      console.log('OK')
+      res.send(results)
+    }
+  })
+})
+
+app.use('/client', express.static(path.join(__dirname, './client')));
 
 app.listen(config.port)
 console.log('API Magic happens on port ' + config.port)
