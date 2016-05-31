e2f7a29cd4d2870928a52d1c89a06daed97b6592
Craig Bojko <craig@bojko.co.uk>
2016-05-31 22:31:35 +0100

    Fixes to render html in angular app

--- client/public/js/client.js
+++ client/public/js/client.js
@@ -2,4 +2,15 @@ var angular = require('angular')
 var mainController = require('./controllers/main')
 require('angular-route')
 
-angular.module('FsqTestApp', ['ngRoute'])
+angular
+  .module('FsqDemoApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }).controller('MainCtrl', mainController)
